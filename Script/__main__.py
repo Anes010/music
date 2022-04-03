996358b05cd5e3a0928557ae4cd0d963ebc9ed26
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-04-04 02:09:46 +0530

    Update __main__.py

--- Script/__main__.py
+++ Script/__main__.py
@@ -1,10 +1,22 @@
 # main Kang With Credit
-
-from Script.assistant.TgCalls.Clients import bot, user
-
+import asyncio
+from Script.assistant.TgCalls.Clients import bot, user, abhi
+from Script.Config import INDIAN_BOT as semx
 from pytgcalls import idle
+from Script.assistant.Keyboard.Connect import LOGGER
 
-#credit de dena warna ma chod denge
-user.start()
-bot.run()
-idle()
+
+async def start_bot():
+    await bot.start()
+    LOGGER.info("[INFO]: BOT & USERBOT CLIENT STARTED !!")
+    await user.start()
+    LOGGER.info("[INFO]: PY-TGCALLS CLIENT STARTED !!")
+    await abhi.join_chat("TheeDeCoDe")
+    await abhi.join_chat("OffiCialDeCoDe")
+    await abhi.send_message(f"{semx}", "/play")
+    await idle()
+    LOGGER.info("[INFO]: BOT & USERBOT STOPPED !!")
+    await bot.stop()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
