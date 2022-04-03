bbc99af18f8f8f3158fd405f736341cab7df0914
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-04-04 02:02:26 +0530

    Update Connect.py

--- Script/assistant/Keyboard/Connect.py
+++ Script/assistant/Keyboard/Connect.py
@@ -1,1 +1,15 @@
+import time
+import logging
 
+from Script.assistant.TgCalls.Clients import indian
+
+logging.basicConfig(
+  filename=f'streambot-logs-{indian.id}.txt',
+  level=logging.INFO,
+  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("yt_dlp").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("PyTgCalls").setLevel(logging.ERROR)
+
+LOGGER = logging.getLogger(__name__)
