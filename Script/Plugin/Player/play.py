00745d6c792e51a83c98dad2bb96dff82d52ee17
ABHISHEK THAKUR <99659192+MrAbhiX@users.noreply.github.com>
2022-04-07 19:40:39 +0530

    Update play.py

--- Script/Plugin/Player/play.py
+++ Script/Plugin/Player/play.py
@@ -20,7 +20,7 @@ from pytgcalls.types.stream import StreamAudioEnded, StreamVideoEnded
 from pyrogram import filters, Client
 from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
 from pyrogram.types import Message
-from Script.Plugin.Helpers.Player import QUEUE, add_to_queue, get_queue, clear_queue, pop_an_item
+from Script.Plugin.Helpers.Player import QUEUE, add_to_queue, get_queue, clear_queue, pop_an_item, remove_queue
 from Script.Cache.admin_check import *
 from Script.assistant.TgCalls.Clients import bot, user, abhi
 from Script.Cache.YouTubeDL import yt_audio, yt_video
