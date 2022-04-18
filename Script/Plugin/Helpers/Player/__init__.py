2bb67db09d6a82b9314d33c05dbbbfa219498b5c
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-04-18 08:37:48 +0530

    Update __init__.py

--- Script/Plugin/Helpers/Player/__init__.py
+++ Script/Plugin/Helpers/Player/__init__.py
@@ -1,2 +1,18 @@
+
+import re
+import sys
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
 from .queues import QUEUE, add_to_queue, get_queue, clear_queue, pop_an_item
 from .PyTgCalls import skip_current_song, skip_item
+
+# will set it true for functioning of automatic leave
+
+AUTO_LEAVING = getenv("AUTO_LEAVING_ASSISTANT", True)
+
+# Time after which you're assistant account will leave chats automatically.
+
+AUTO_LEAVE_TIME = int(
+    getenv("ASSISTANT_LEAVE_TIME", "1200")
