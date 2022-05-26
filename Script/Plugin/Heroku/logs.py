998b7cd1ab9ad949325687ebe502eecf3ea7aa49
Anes010 <98156562+Anes010@users.noreply.github.com>
2022-05-26 13:10:31 +0300

    Update logs.py

--- Script/Plugin/Heroku/logs.py
+++ Script/Plugin/Heroku/logs.py
@@ -8,10 +8,10 @@ from Script.Plugin.Helpers.Heroku import edit_or_send_as_file
 from pyrogram.types import Message
 
 
-@Client.on_message(filters.command(["logs"]) & filters.user(OWNER_ID))
+@Client.on_message(filters.command(["سجلات"]) & filters.user(OWNER_ID))
 @_check_heroku
 async def logs(client: Client, message: Message, happ):
     msg = await message.reply_text("`please wait for a moment!`")
     logs = happ.get_log()
     capt = f"Heroku logs of `{HEROKU_APP_NAME}`"
-    await edit_or_send_as_file(logs, msg, client, capt, "logs")
+    await edit_or_send_as_file(logs, msg, client, capt, "سجلات")
