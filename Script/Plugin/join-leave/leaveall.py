9d1b860b3b2f72e1f5e99c4554179be78b9f5126
Anes010 <98156562+Anes010@users.noreply.github.com>
2022-05-26 12:22:24 +0300

    Update leaveall.py

--- Script/Plugin/join-leave/leaveall.py
+++ Script/Plugin/join-leave/leaveall.py
@@ -4,23 +4,23 @@ from Script.Config import OWNER_ID
 from Script.assistant.TgCalls.Clients import abhi
 
 
-@Client.on_message(filters.command(["userbotleaveall", "leaveall"]) & filters.user(OWNER_ID))
+@Client.on_message(filters.command(["غادر", "leaveall"]) & filters.user(OWNER_ID))
 async def leaveall(client, message):
     if message.from_user.id not in OWNER_ID:
         return
 
     left = 0
     failed = 0
-    lol = await message.reply("Assistant Leaving all chats")
+    lol = await message.reply("غادر كل المجموعات")
     async for dialog in abhi.iter_dialogs():
         try:
             await abhi.leave_chat(dialog.chat.id)
             left += 1
             await lol.edit(
-                f"Assistant leaving all group... \n\nLeft: {left} chats. Failed: {failed} chats."
+                f"يتم المغادره... \n\nLeft: {left} chats. Failed: {failed} chats."
             )
         except:
             failed += 1
-            await lol.edit(f"Assistant leaving... Left: {left} chats. Failed: {failed} chats.")
+            await lol.edit(f"يتم المغادره... Left: {left} chats. Failed: {failed} chats.")
         await asyncio.sleep(0.7)
     await client.send_message(message.chat.id, f"Left {left} chats. Failed {failed} chats.")
